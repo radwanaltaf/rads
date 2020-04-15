fc0e74d4c005662e8caab6d3bd4a2522fd30ae46
radwanaltaf <radwanaltaf2000@gmail.com>
2020-04-16 03:19:32 +0500

    adding a table to the body section

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,8 +16,29 @@ class Body extends Component {
                         <div className=" grow bodyDiv">
                             <div className="shadow-1 innerBodyLeft">
                                 <div className="aboutMe">
-                                    <p className="bodyTxt">{bodyTxt[1]}</p>
-                                    <p className="bodyTxt">{bodyTxt[2]}</p>
+                                    <p className="bodyTxt">{bodyTxt[1]} </p>
+                                    {/* <p className="bodyTxt">{bodyTxt[2]}</p> */}
+                                    <p className="bodyTxt">{bodyTxt[3]}</p>
+                                    <div className="bodyGrid">
+                                        <div className="tc">
+                                            <h3 className="ma0 white">
+                                                Front-End
+                                            </h3>
+                                           <p className="lightgray ma0 mt2 txtAlign">
+                                               AngularJS
+                                           </p>
+                                           <p className="white ma0 mt2 txtAlign">
+                                               ReactJS
+                                           </p>
+                                        </div>
+                                        <div className="tc">
+                                            <h3 className="ma0 white">
+                                                Back-End
+                                            </h3>
+                                        </div>
+
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
